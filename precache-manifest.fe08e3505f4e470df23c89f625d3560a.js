b70c24b427a9767913b836f41e6ab3c4d490fdaf
Michael Nico Suarez <michaelnicosuarez@gmail.com>
2019-05-04 20:47:10 +0800

    Updates

---
+++ precache-manifest.fe08e3505f4e470df23c89f625d3560a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b04cf893bd450af9563c5925795a96f",
+    "url": "/helpbuddy/index.html"
+  },
+  {
+    "revision": "bf519704c6d6f4a267c1",
+    "url": "/helpbuddy/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/helpbuddy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "bf519704c6d6f4a267c1",
+    "url": "/helpbuddy/static/js/main.3030f439.chunk.js"
+  },
+  {
+    "revision": "5e388ff55f4680a89d0d",
+    "url": "/helpbuddy/static/js/runtime~main.2112dabe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/helpbuddy/static/media/logo.5d5d9eef.svg"
+  }
+]);
